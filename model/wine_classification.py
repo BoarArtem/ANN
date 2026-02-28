33a77e80c8a0216700942cdc805d18c52d8de0cf
unknown <workboar@gmail.com>
2026-03-01 01:00:05 +0200

    Added StandardScaler

--- model/wine_classification.py
+++ model/wine_classification.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.callbacks import EarlyStopping
 
 data = pd.read_csv("../data/WineQT.csv")
 data = data.drop(['Id'], axis=1)
@@ -14,16 +14,19 @@ y = y - 3
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
 model = Sequential([
     Dense(64, activation='relu', input_shape=(X_train.shape[1], )), # Hidden layer 1
-    Dense(64, activation='relu'), # Hidden layer 2
+    Dense(32, activation='relu'), # Hidden layer 2
 
     Dense(6, activation='softmax') # Output layer
 ])
 
 opt=tf.keras.optimizers.Adam(learning_rate=0.001)
 loss=tf.keras.losses.SparseCategoricalCrossentropy()
-# early_stopping=EarlyStopping(monitor='val_loss', patience=20)
 
 model.compile(optimizer=opt, loss=loss, metrics=["accuracy"])
 model.fit(X_train, y_train, epochs=200, validation_data=(X_test, y_test))
